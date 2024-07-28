98e2aa7156257ed21d6958d4ec9c831e5cf63b96
Manojkumar-05 <manojkumar.cb22@bitsahty.ac.in>
2024-07-28 08:04:37 +0530

    First Commit...

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Component } from "./components/component/component"
+
+const App = () => {
+  return (
+    <>
+    <Component />
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // plugins: [nextui()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": "/src/components",
+      "@components/ui": "/src/components/ui",
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      css: {
+        // Make sure to adjust the path if necessary
+        additionalData: `@import "@nextui-org/react/dist/nextui.css";`,
+      },
+    },
+  },
+});
