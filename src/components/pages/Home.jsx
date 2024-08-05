bdc474d81a37635bdfb9313269907e8834bfb2e9
Manojkumar-05 <manojkumar.cb22@bitsahty.ac.in>
2024-08-05 15:17:16 +0530

    colors

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,7 +2,7 @@ import { Hero } from "../ui/Hero";
 
 const Home = () => {
   return (
-    <div className="bg-[#460073] min-h-screen overflow-auto">
+    <div className="bg-[#470073] min-h-screen overflow-auto">
       <Hero />
     </div>
   );
