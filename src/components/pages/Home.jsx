bb792fb2363e7b75fd85be61019744a6d747841a
Manojkumar-05 <manojkumar.cb22@bitsahty.ac.in>
2024-08-05 15:40:39 +0530

    color

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,11 @@
+import { BackgroundBeams } from "../ui/ui-comp/background-beams";
 import { Hero } from "../ui/Hero";
 
 const Home = () => {
   return (
     <div className="bg-[#460073] min-h-screen overflow-auto">
       <Hero />
+      <BackgroundBeams />
     </div>
   );
 };
